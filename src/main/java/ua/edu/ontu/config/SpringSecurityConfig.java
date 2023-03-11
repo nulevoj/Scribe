ffd71c731e81ab00b5a4c20d1c7bd92cd56c96b7
Nulevoj <siechinyurii@gmail.com>
2023-03-11 19:00:11 +0200

    refactoring

--- src/main/java/ua/edu/ontu/config/SpringSecurityConfig.java
+++ src/main/java/ua/edu/ontu/config/SpringSecurityConfig.java
@@ -21,7 +21,7 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                //.csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/authentication/**", "/css/**").permitAll()
