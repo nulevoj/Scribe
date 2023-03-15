e3bef9be108852d122eefeccc8fe73b2e5e8e038
Nulevoj <siechinyurii@gmail.com>
2023-03-15 22:05:22 +0200

    implement uploading new file;

--- src/main/java/ua/edu/ontu/controller/DocumentController.java
+++ src/main/java/ua/edu/ontu/controller/DocumentController.java
@@ -4,8 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ua.edu.ontu.model.entity.Document;
 import ua.edu.ontu.service.DocumentService;
+import ua.edu.ontu.service.FileService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Controller
 @RequestMapping("/document")
@@ -13,6 +19,9 @@ public class DocumentController {
 
     @Autowired
     private DocumentService documentService;
+
+    @Autowired
+    private FileService fileService;
 
     @GetMapping
     public String allDocumentsPage(Model model) {
@@ -28,14 +37,35 @@ public class DocumentController {
     }
 
     @GetMapping("/new")
-    public String newPage(Model model) {
-        model.addAttribute("document", new Document());
+    public String newPage() {
         return "document/new";
     }
 
     @PostMapping
-    public String create(@ModelAttribute("document") Document document) {
-        documentService.save(document);
+    public String create(@RequestParam("file") MultipartFile file,
+                         @RequestParam("description") String description) {
+        if (file.isEmpty()) {
+            return "redirect:/document/new?emptyFile";
+        }
+        Document document;
+        document = documentService.findByFileName(file.getOriginalFilename());
+        if (document != null) {
+            return "redirect:/document/new?fileExist";
+        }
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = fileService.createInSourcePath(file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            document = new Document();
+            document.setFileName(file.getOriginalFilename());
+            document.setDescription(description);
+            documentService.save(document);
+
+            return "redirect:/document?success";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "redirect:/document";
     }
 
--- src/main/java/ua/edu/ontu/service/FileService.java
+++ src/main/java/ua/edu/ontu/service/FileService.java
@@ -1,21 +1,27 @@
-package ua.edu.ontu.scribe;
+package ua.edu.ontu.service;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-@Component
-public class DefaultDirectoryFileFactory {
+@Service
+public class FileService {
 
-    @Value("scribe.source-path")
+    @Value("${scribe.source-path}")
     private String sourcePath;
 
-    @Value("scribe.output-path")
+    @Value("${scribe.output-path}")
     private String outputPath;
 
     public File createFromSourcePath(String fileName) {
         return new File(sourcePath + fileName);
+    }
+
+    public Path createInSourcePath(String fileName) {
+        return Paths.get(sourcePath, fileName);
     }
 
     public File createInOutputPath(String fileName) {
