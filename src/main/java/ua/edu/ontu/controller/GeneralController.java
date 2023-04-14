29f6687615af97429f8dc52388d3020246ec1bba
Nulevoj <siechinyurii@gmail.com>
2023-04-15 00:10:32 +0300

    add placeholders to 'placeholders' page;

--- src/main/java/ua/edu/ontu/controller/GeneralController.java
+++ src/main/java/ua/edu/ontu/controller/GeneralController.java
@@ -1,7 +1,15 @@
 package ua.edu.ontu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.edu.ontu.model.entity.Account;
+import ua.edu.ontu.model.entity.Employee;
+import ua.edu.ontu.model.entity.Student;
+import ua.edu.ontu.scribe.Vocabulary;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Controller
 public class GeneralController {
@@ -22,7 +30,19 @@ public class GeneralController {
     }
 
     @GetMapping("/placeholders")
-    public String placeholdersPage() {
+    public String placeholdersPage(Model model) {
+        Account account = new Account();
+        Vocabulary vocabulary = new Vocabulary();
+
+        account.setPerson(new Student());
+        vocabulary.putAll(account);
+
+        account.setPerson(new Employee());
+        vocabulary.putAll(account);
+
+        Set<String> set = new LinkedHashSet<>();
+        vocabulary.getMap().keySet().stream().sorted().forEach(set::add);
+        model.addAttribute("set", set);
         return "general/placeholders";
     }
 
